ff79333a38bba944a8e614e8a173893aed3bfe41
vladimirpetukhov <vladimir.petukhov@semansys.com>
2021-05-23 13:49:25 +0300

    ADD: update version

---
+++ src/configs/mongo.config.ts
@@ -0,0 +1,20 @@
+import { ConfigService } from '@nestjs/config';
+import { TypegooseModuleOptions } from 'nestjs-typegoose';
+
+export const getMongoConfig = async (
+  configService: ConfigService,
+): Promise<TypegooseModuleOptions> => {
+  return {
+    uri: getMongoString(configService),
+    ...getMongoOptions(),
+  };
+};
+
+const getMongoString = (configService: ConfigService) =>
+  'mongodb://localhost:27017';
+
+const getMongoOptions = () => ({
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
